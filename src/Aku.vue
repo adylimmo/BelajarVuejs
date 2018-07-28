a51c791a60d0960dfa6f93f6a22f8cf9c78183ef
ady.limmo <ady.limmo@gmail.com>
2018-07-29 00:03:33 +0700

    Belajar Vue.js Pertama

---
+++ src/Aku.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <i>{{aku}}</i>
+        </div>
+</template>
+<script>
+export default {
+    data () {
+        return {
+            aku:'halooooo'
+        }
+    }
+}
+</script>
---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <b>{{msg}}</b>
+        <aku></aku>
+        </div>
+</template>
+<script>
+import Aku from './Aku.vue'
+export default {
+    components: {
+        'aku':Aku
+    },
+    data() {
+        return {
+            msg:'Selamat Gabung'
+        }
+    }
+}
+</script>
+<style></style>
